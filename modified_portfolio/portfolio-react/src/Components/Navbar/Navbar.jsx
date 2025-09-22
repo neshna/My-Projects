3ba8590c9e7827d9cb3d7c5ccfd2e322b95e6fb8
Neshana Priya <neshana2312@gmail.com>
2025-09-22 21:22:12 +0530

    Modified-Portfolio

---
+++ modified_portfolio/portfolio-react/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,89 @@
+import React, { useState, useRef } from 'react'
+import './Navbar.css'
+import theme_pattern from '../../assets/theme_pattern.svg'
+import underline from '../../assets/nav_underline.svg'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+import menu_close from '../../assets/menu_close.svg'
+import menu_open from '../../assets/menu_open.svg'
+
+const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false)
+  const [menu, setMenu] = useState("")
+  const menuRef = useRef();
+
+  const openMenu = () => {
+    menuRef.current.style.right = "0";
+    
+  }
+  const closeMenu = () => {
+    menuRef.current.style.right = "-100%";
+    
+  }
+
+  return (
+    <div className="navContainer">
+      <div className="logo">
+        <h1>NesH</h1>
+        <img src={theme_pattern} alt="Logo" className="theme_img" />
+      </div>
+
+      {/* Mobile hamburger */}
+      {showMenu || (<img
+        src={menu_open}
+        alt="menu"
+        className="nav-mob-open"
+        onClick={() => {setShowMenu(true);}}
+      />)}
+
+      {showMenu && (
+        <ul className="navLinks" ref={menuRef}>
+          {/* Close button inside menu */}
+          <img
+            src={menu_close}
+            alt="close"
+            className="nav-mob-close"
+            onClick={() => { setShowMenu(false) }}
+          />
+          <li>
+            <AnchorLink className="AnchorL-Link" offset={200} href="#About" onClick={() => { setMenu("About Me"); openMenu; }}>
+              <p>About Me</p>
+            </AnchorLink>
+            {menu === "About Me" && <img src={underline} alt="underline" />}
+          </li>
+
+          <li>
+            <AnchorLink className="AnchorL-Link" offset={150} href="#Skills" onClick={() => { setMenu("Skills"); openMenu; }}>
+              <p>Skills</p>
+            </AnchorLink>
+            {menu === "Skills" && <img src={underline} alt="underline" />}
+          </li>
+
+          <li>
+            <AnchorLink className="AnchorL-Link" offset={150} href="#Projects" onClick={() => { setMenu("Projects"); openMenu; }}>
+              <p>Projects</p>
+            </AnchorLink>
+            {menu === "Projects" && <img src={underline} alt="underline" />}
+          </li>
+
+          <li>
+            <AnchorLink className="AnchorL-Link" offset={150} href="#Contact" onClick={() => { setMenu("Contact"); openMenu; }}>
+              <p>Contact</p>
+            </AnchorLink>
+            {menu === "Contact" && <img src={underline} alt="underline" />}
+          </li>
+        </ul>
+      )}
+
+
+      {/* Desktop Button */}
+      <div className="navButton">
+        <AnchorLink className="AnchorL-Link" offset={150} href="#Contact">Connect With Me</AnchorLink>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
+
+
+
---
+++ modified_portfolio/portfolio-react/src/assets/mywork_data.js
@@ -0,0 +1,48 @@
+import project1_img from '../assets/project_1.png'
+import project2_img from '../assets/project_2.png'
+import project3_img from '../assets/project_3.png'
+import project4_img from '../assets/project_4.png'
+import project5_img from '../assets/project_5.png'
+import project6_img from '../assets/project_6.png'
+
+const mywork_data = [
+    {
+        w_no:1,
+        w_name:"YouTube Clone",
+        w_img:project1_img,
+        languages: ["HTML" ,"CSS"]
+
+    },
+    {
+        w_no:2,
+        w_name:"Todo List",
+        w_img:project2_img,
+        languages: ["HTML" , "CSS" , "JavaScript"]
+    },
+    {
+        w_no:3,
+        w_name:"Rock Paper Scissor",
+        w_img:project3_img,
+        languages: ["HTML" , "CSS" , "JavaScript"]
+    },
+    {
+        w_no:4,
+        w_name:"Amazon Clone",
+        w_img:project4_img,
+        languages: ["HTML" , "CSS" , "JavaScript"]
+    },
+    {
+        w_no:5,
+        w_name:"Todo List",
+        w_img:project5_img,
+        languages: ["HTML" , "CSS" , "JavaScript" , "React"]
+    },
+    {
+        w_no:6,
+        w_name:"Note Keeper App",
+        w_img:project6_img,
+        languages:["HTML" , "CSS" , "JavaScript" , "React"]
+    }
+]
+ 
+export default mywork_data;
